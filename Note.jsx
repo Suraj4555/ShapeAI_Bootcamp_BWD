702e133431270befc3ceec5c0638aa4d31bdbcbf
Suraj4555 <85476219+Suraj4555@users.noreply.github.com>
2021-06-07 11:32:54 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> Bootcamp offered by Shape AI </h1>
+      <p>
+        Amazing bootcamp!
+        <br />
+        great experience!!
+      </p>
+    </div>
+  );
+}
+
+export default Note;
